9c35745783da8f219312db6b61ce58a36a3a8e94
asuarez <asuarez@106-28.info2>
2023-10-03 14:36:56 +0200

    Boletín 1 Tema 2

---
+++ BoletinTema2/src/boletin1/Ejercicio2.java
@@ -0,0 +1,35 @@
+package boletin1;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// Definimos las variables donde se almacenarán los números
+		int num1, num2;
+		
+		// Creamos el Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Le pedimos al usuario el primer número y lo leemos
+		System.out.println("Introduzca un número:");
+	
+		num1 = sc.nextInt();
+		
+		// Le pedimos al usuario el segundo número y lo leemos
+		System.out.println("Introduzca otro número:");
+		
+		num2 = sc.nextInt();
+		
+		// Comprobamos si son iguales y mostramos el mensaje por pantalla
+		if (num1==num2) {
+			System.out.println("Los números son iguales");
+		} else {
+			System.out.println("Los números son diferentes");
+		}
+		 
+		// Cerramos el Scanner
+		sc.close();
+	}
+	
+}
---
+++ BoletinTema2/src/boletin1/Ejercicio3.java
@@ -0,0 +1,29 @@
+package boletin1;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// Definimos la variable donde se almacenará el número decimal
+		float num;
+		
+		// Creamos el Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Le pedimos al usuario el número y lo leemos
+		System.out.println("Introduzca un número con decimales");
+		
+		num = sc.nextFloat();
+		
+		if (num < 1 && num > -1 && num != 0) {
+			System.out.println("Tu número es casi cero");
+		} else {
+			System.out.println("Tu número no es casi cero");
+		}
+		
+		// Cerramos el Scanner
+		sc.close();
+	}
+	
+}
