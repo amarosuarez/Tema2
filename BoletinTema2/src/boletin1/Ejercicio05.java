0245de186ec8b0442bc949a9308993a36a4e1f49
Amaro Suárez <amaro.suarez@iesnervion.es>
2023-10-06 14:34:31 +0200

    Boletin 1 Tema 2

--- BoletinTema2/src/boletin1/Ejercicio05.java
+++ BoletinTema2/src/boletin1/Ejercicio05.java
@@ -1,9 +1,5 @@
 package boletin1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Ejercicio05 {
