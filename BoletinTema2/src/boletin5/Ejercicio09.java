5cba1a32c41d55f1723a730e4c88c11166261743
Amaro Suárez <amaro.suarez@iesnervion.es>
2023-10-31 13:55:40 +0100

    Cambios

--- BoletinTema2/src/boletin5/Ejercicio09.java
+++ BoletinTema2/src/boletin5/Ejercicio09.java
@@ -17,21 +17,28 @@ public class Ejercicio09 {
 		System.out.println("Introduzca un número");
 		num = sc.nextInt();
 		
-		// Primero se comprueba si es 1
-		if (num==1) {
-			isPrimo = false;
-		} else {
-			// Si no es 1 se comprueba si es primo o no
-			for (var i=2; i<num; i++) {
-				 if (num%i==0) {
-					isPrimo = false;
-					break;
+		// Se comprueba que el número sea mayor que 0
+		if (num > 0) {
+			// Primero se comprueba si es 1
+			if (num==1) {
+				isPrimo = false;
+			} else {
+				// Si no es 1 se comprueba si es primo o no
+				for (var i=2; i<num; i++) {
+					 if (num%i==0) {
+						isPrimo = false;
+						break;
+					}
 				}
 			}
-		}
+			
+			// Se muestra el mensaje por la pantalla
+			System.out.println(isPrimo ? "Es primo" : "No es primo");
+		} else 
+			// El número es incorrecto ya que es 0 o menor
+			System.out.println("El número introducido es incorrecto");
 		
-		// Se muestra el mensaje por la pantalla
-		System.out.println(isPrimo ? "Es primo" : "No es primo");
+		
 
 		
 		// Cerramos el Scanner
